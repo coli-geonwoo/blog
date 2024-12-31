b78f36276952f9ce02abb8680f8d4b633b26092f
coli <kkwoo001021@naver.com>
2024-12-31 14:16:26 -0800

    test: displayName 작성

--- blog/src/test/java/com/example/blog/service/CompanyServiceTest.java
+++ blog/src/test/java/com/example/blog/service/CompanyServiceTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import com.example.blog.entity.Company;
 import com.example.blog.entity.Member;
 import com.example.blog.repository.CompanyRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,6 +24,7 @@ class CompanyServiceTest {
     @Autowired
     private MemberRepository memberRepository;
 
+    @DisplayName("batch 쿼리를 동기화하지 않으면 에러가 발생한다.")
     @Test
     void deleteCompanyWithoutClear() {
         Company company = companyRepository.save(new Company());
@@ -32,6 +34,7 @@ class CompanyServiceTest {
                 .isInstanceOf(InvalidDataAccessApiUsageException.class);
     }
 
+    @DisplayName("batch 쿼리 후, clear로 영속성 컨텍스트를 비워주면 에러가 발생하지 않는다.")
     @Test
     void deleteCompanyWithClear() {
         Company company = companyRepository.save(new Company());
