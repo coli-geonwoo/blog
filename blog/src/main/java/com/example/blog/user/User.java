a83b6a96c30c1a34d81c66d7488b2ee2119586e0
coli <kkwoo001021@naver.com>
2025-05-01 01:50:01 +0900

    feat: 기초 데이터 추가

---
+++ blog/src/main/java/com/example/blog/user/User.java
@@ -0,0 +1,21 @@
+package com.example.blog.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
---
+++ blog/src/main/java/com/example/blog/user/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.blog.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
